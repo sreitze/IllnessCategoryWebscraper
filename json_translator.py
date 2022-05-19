6a2bc7c850d17c5a10dda89211e56bdce5f318fb
sreitze <sreitze@uc.cl>
2022-05-19 14:53:50 -0400

    ♻️ adding category list instead of scraping

--- json_translator.py
+++ json_translator.py
@@ -4,8 +4,10 @@ from googletrans import Translator
 translator = Translator()
 data = json.load(open('active_principles.json'))
 string = json.dumps(data)
+
 translate = translator.translate(string, src='en', dest='es')
-dict = json.loads(translate.text)
+text = translate.text
+dict = json.loads(text)
 
 with open('active_principles_es.json', 'w') as f:
     json.dump(dict, f, indent=4)
--- main.py
+++ main.py
@@ -4,7 +4,13 @@ from scaper import Scraper
 if __name__ == "__main__":
   scraper = Scraper()
   active_principles = []
-  categories = scraper.find_categories()
+  categories = ['Allergy & Cold', 'Anesthetics', 'Antidotes', 'Antimicrobials', 
+                'Blood Components', 'Cardiovascular', 'Critical Care', 'Dental & Oral Care', 
+                'Dermatologics', 'Gastrointestinal', 'Hematologics', 'Herbals & Supplements', 
+                'Imaging Agents', 'Immunologics', 'Metabolic & Endocrine', 'Neurologics', 
+                'Nutritionals', 'Oncology', 'Ophthalmics', 'Otics', 
+                'Pain Management', 'Psychiatrics', 'Pulmonary', 'Rheumatologics', 
+                'Urologics', 'Vaccinations', "Women's Health & Reproduction"]
                 
   for category in categories:
     sub_categories = scraper.find_sub_categories(category)
